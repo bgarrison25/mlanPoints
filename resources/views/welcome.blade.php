b38746bd00a9a9a041851b08fa71f9a055fb7595
Bill <william.garrison@nrg.com>
2020-01-08 18:54:03 +0000

    remove home link from main page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,14 +65,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
                     MassiveLAN Cup Points
